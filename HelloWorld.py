d3e5301ed88d88b9726814c52accdbec9d8bf7cd
Salman Ali <sali@zetron.com>
2022-05-19 11:41:24 -0700

    first function

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,11 @@
 # HelloWorld.py
 
+def first_func():
+    print("Inside first function!\n")
+    
 def main():
     print("Hello World Zetron!")
+    first_func()
 
 if __name__ == "__main__":
     main()
