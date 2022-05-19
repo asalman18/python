2587587240f3aaa009567c7960953dc068af0276
Jared Ellerbrock <jellerbrock@zetron.com>
2022-05-19 16:30:51 -0700

    Jared's first update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,12 +1,16 @@
 # HelloWorld.py
 
+def jared_first_func():
+    print("This is Jared's first function.")
+    
 def first_func():
     print("Inside first function!\n")
     
 def main():
     print("Hello World Zetron!")
     first_func()
-
+    jared_first_func()
+    
 if __name__ == "__main__":
     main()
 
